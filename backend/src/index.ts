22f155b9cbbc20d69b8e7d7cb7bfe6d799826e3c
Daniel Gacitúa <daniel.gacitua@usach.cl>
2024-04-16 15:03:42 -0400

    feat: Add Docker image support for backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,12 @@
 import express, { Express, Request, Response } from 'express'
 
 const app: Express = express()
-const port = 3000
+const port = 3001
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, this is Express + TypeScript')
 })
 
 app.listen(port, () => {
-  console.log(`[Server]: I am running at https://localhost:${port}`)
+  console.log(`[Server] I am running at http://localhost:${port}`)
 })
